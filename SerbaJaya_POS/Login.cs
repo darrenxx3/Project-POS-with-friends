625e25156e5b4eef850e27245af59a1b8e46a33c
vincencius <vincenciuschristiano.vc@gmail.com>
2022-04-27 15:09:58 +0700

    update login

--- SerbaJaya_POS/Login.cs
+++ SerbaJaya_POS/Login.cs
@@ -21,7 +21,90 @@ namespace SerbaJaya_POS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("NASI REBUS!");
+
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+
+            var conn = new Connection.Connection_Query();
+
+            try
+            {
+                conn.OpenConnection();
+
+                string query =
+                    "SELECT p.PositionName , e.EmployeeName FROM Employee e " +
+                    "INNER JOIN Position p ON p.PositionID = e.PositionID " +
+                    "WHERE e.EmployeeID ='" + tbUsername.Text + "' AND " +
+                    "e.Password ='" + tbPassword.Text + "'";
+
+                SqlDataReader dr = conn.DataReader(query);
+
+                if (dr.Read())
+                {
+                    this.Hide();
+                    Parent parent = new Parent(dr.GetString(0), dr.GetString(1));
+                    parent.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong username/password");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error! Please kindly report to officer: " + ex);
+            }
+
+                {
+                    MessageBox.Show("Wrong username/password");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error! Please kindly report to officer: " + ex);
+            }
+
+                {
+                    MessageBox.Show("Wrong username/password");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error! Please kindly report to officer: " + ex);
+            }
+
+                {
+                    MessageBox.Show("Wrong username/password");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error! Please kindly report to officer: " + ex);
+            }
+
+                {
+                    MessageBox.Show("Wrong username/password");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error! Please kindly report to officer: " + ex);
+            }
+
+                {
+                    MessageBox.Show("Wrong username/password");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Database Error! Please kindly report to officer: " + ex);
+            }
+
         }
     }
 }
