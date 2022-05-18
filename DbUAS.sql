d969dfcb97efac99e8f7e135ae5b5f8db4a27c20
kstnecniv <vincentsk0703@gmail.com>
2022-05-18 16:32:05 +0700

    t

--- DbUAS.sql
+++ DbUAS.sql
@@ -18,7 +18,8 @@ create table Employee
 	PositionID char(10), foreign key (PositionID) references Position,
 	Address varchar(100) NOT NULL,
 	PhoneNumber char(20) NOT NULL,
-	Password varchar(100) NOT NULL
+	Password varchar(100) NOT NULL,
+	IsNotEmployee varchar(20) NOT NULL
 );
 
 create table DataItem
@@ -28,7 +29,8 @@ create table DataItem
 	Cost int not null,
 	SalesPrice int not null,
 	Stock int not null,
-	Descriptions varchar(50) not null
+	Descriptions varchar(50) not null,
+	IsDiscontinued varchar(20) NOT NULL
 );
 
 create table Supplier
@@ -36,7 +38,8 @@ create table Supplier
     SupplierID char(10) primary key not null,
     SupplierName varchar(50) not null,
     SupplierAddress varchar(50) not null,
-    SupplierPhone varchar(50) not null
+    SupplierPhone varchar(50) not null,
+	IsActive varchar(20) not null
 );
 
 CREATE TABLE Sales(
@@ -49,6 +52,7 @@ CREATE TABLE SalesDetail(
     SalesID CHAR(10) FOREIGN KEY REFERENCES Sales(SalesID) NOT NULL,
     ItemID CHAR(10) FOREIGN KEY REFERENCES DataItem(ItemID) NOT NULL,
     Quantity INT NOT NULL,
+	salePrice INT NOT NULL
 );
 
 CREATE TABLE PurchaseOrder(
@@ -56,9 +60,10 @@ CREATE TABLE PurchaseOrder(
     SupplierID CHAR(10) FOREIGN KEY REFERENCES Supplier(SupplierID) NOT NULL,
     OrderDate DATE NOT NULL,
     EmployeeID CHAR(10) FOREIGN KEY REFERENCES Employee(EmployeeID) NOT NULL,
-	OrderStatus CHAR(20) NOT NULL,
+	OrderStatus VARCHAR(30) NOT NULL,
 	WarehouseNote VARCHAR(100)
 );
+
 
 CREATE TABLE PurchaseOrderDetail(
     PurchaseOrderID CHAR(10) FOREIGN KEY REFERENCES PurchaseOrder(PurchaseOrderID) NOT NULL,
@@ -75,30 +80,30 @@ insert into Position
 	('WH001', 'Warehouse');
 
 insert into Employee 
-	(EmployeeID, EmployeeName, PositionID, Address, PhoneNumber, Password) 
+	(EmployeeID, EmployeeName, PositionID, Address, PhoneNumber, Password, IsNotEmployee) 
 	values 
-	('E0001', 'Agustinus Budiman', 'WH001','Jl. pagi sore sore juga boleh', '082269696969', '123'),
-	('E0002', 'Calvin Hartanto', 'MM001','Jl. pagi sore sore juga boleh', '082269696969', '123'),
-	('E0003', 'David Sihombing', 'CC001','Jl. pagi sore sore juga boleh', '082269696969', '123'),
-	('E0004', 'Tsucipto Purnomo', 'CC001','Jl. pagi sore sore juga boleh', '082269696969', '123');
+	('E0001', 'Agustinus Budiman', 'WH001','Jl. pagi sore sore juga boleh', '082269696969', '123', 'false'),
+	('E0002', 'Calvin Hartanto', 'MM001','Jl. pagi sore sore juga boleh', '082269696969', '123', 'false'),
+	('E0003', 'David Sihombing', 'CC001','Jl. pagi sore sore juga boleh', '082269696969', '123', 'false'),
+	('E0004', 'Tsucipto Purnomo', 'CC001','Jl. pagi sore sore juga boleh', '082269696969', '123', 'false');
 
 insert into DataItem
-	(ItemID, ItemName, Cost, SalesPrice, Stock, Descriptions)
+	(ItemID, ItemName, Cost, SalesPrice, Stock, Descriptions, IsDiscontinued)
 	values
-	('MK0123', 'Sosis So Nice', 69000, 75000, 15, 'Sosis'),
-	('MK0124', 'Kiko', 20000, 25000, 20, 'Dodol Keras'),
-	('MK0125', 'Lifegurl', 15000, 20000, 3000, 'Sabun'),
-	('MK0126', 'Sarimie', 30000, 35000, 4000, 'Mie Instan'),
-	('MK0127', 'Sabun', 20000, 25000, 30, 'Sabun Mandi');
+	('MK0123', 'Sosis So Nice', 69000, 75000, 15, 'Sosis', 'false'),
+	('MK0124', 'Kiko', 20000, 25000, 20, 'Dodol Keras', 'false'),
+	('MK0125', 'Lifegurl', 15000, 20000, 3000, 'Sabun', 'false'),
+	('MK0126', 'Sarimie', 30000, 35000, 4000, 'Mie Instan', 'false'),
+	('MK0127', 'Sabun', 20000, 25000, 30, 'Sabun Mandi', 'false');
 
 insert into Supplier
-    (SupplierID, SupplierName, SupplierAddress, SupplierPhone)
+    (SupplierID, SupplierName, SupplierAddress, SupplierPhone, IsActive)
     values
-    ('SS001','PT. Japfa','Jl. Sudirman','021-5524371'),
-    ('SS002','PT. Bucin','Jl. Rasuna Said','021-5524372'),
-    ('SS003','PT. Alfa','Jl. Merdeka','021-5524373'),
-    ('SS004','PT. JayaEsport','Jl.Grogol','021-5524374'),
-    ('SS005','PT. Bersinar ','Jl. Hasyim Ashari','021-5524375');
+    ('SS001','PT. Japfa','Jl. Sudirman','021-5524371', 'true'),
+    ('SS002','PT. Bucin','Jl. Rasuna Said','021-5524372', 'true'),
+    ('SS003','PT. Alfa','Jl. Merdeka','021-5524373', 'true'),
+    ('SS004','PT. JayaEsport','Jl.Grogol','021-5524374', 'true'),
+    ('SS005','PT. Bersinar ','Jl. Hasyim Ashari','021-5524375', 'true');
 
 INSERT INTO Sales (SalesID,TransactionDate,EmployeeID)
 VALUES
@@ -108,13 +113,13 @@ VALUES
     ('S0004', '1/2/2022 12:00:00', 'E0004'),
     ('S0005', '1/2/2022 08:00:00', 'E0004');
 
-INSERT INTO SalesDetail (SalesID,ItemID,Quantity)
+INSERT INTO SalesDetail (SalesID,ItemID,Quantity, salePrice)
 VALUES
-    ('S0001', 'MK0123', '1'),
-    ('S0002', 'MK0124', '2'),
-    ('S0003', 'MK0125', '3'),
-    ('S0005', 'MK0126', '4'),
-    ('S0005', 'MK0127', '5');
+    ('S0001', 'MK0123', '1', 12000),
+    ('S0002', 'MK0124', '2', 12000),
+    ('S0003', 'MK0125', '3', 12000),
+    ('S0005', 'MK0126', '4', 12000),
+    ('S0005', 'MK0127', '5', 12000);
 
 INSERT INTO PurchaseOrder (PurchaseOrderID,SupplierID,OrderDate,EmployeeID, OrderStatus, WarehouseNote)
 VALUES
